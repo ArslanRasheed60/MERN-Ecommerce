3842200655f2f098dc893faa7b2cce4c935a1b94
Arslan Rasheed <arslanrashee1@gmail.com>
2023-07-24 23:39:27 -0700

    file upload done

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ import connectDB from './config/db.ts'
 import productRoutes from './routes/productRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 import orderRoutes from './routes/orderRoutes.ts'
-// import uploadRoutes from './routes/uploadRoutes.ts'
+import uploadRoutes from './routes/uploadRoutes.ts'
 
 
 dotenv.config()
@@ -31,15 +31,13 @@ app.use(cors())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
-// app.use('/api/upload', uploadRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-// const __dirname = path.resolve()
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '/frontend/build')))
 
