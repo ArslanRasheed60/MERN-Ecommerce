a1ca4093b2c68476984381a609526f5b929762f7
Arslan Rasheed <arslanrashee1@gmail.com>
2023-07-25 04:49:22 -0700

    backend updated

--- backend/server.ts
+++ backend/server.ts
@@ -39,16 +39,22 @@ app.get('/api/config/paypal', (req, res) =>
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  // app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  )
-} else {
+  // app.get('*', (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  // )
+  app.use(express.static("frontend/build"));
+  app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  })
+}
+else {
   app.get('/', (req: Request, res: Response) => {
     res.send('API is running....')
   })
 }
+
 
 
 //custom middlewares called after the url 
